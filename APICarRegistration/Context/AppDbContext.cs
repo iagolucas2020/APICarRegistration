64b395220fc7b10e03e6712abeeca1e4ad84d3bc
Iago <iago0905@hotmail.com>
2023-07-12 09:58:20 -0300

    Implementação da conexão com o sqlServer

---
+++ APICarRegistration/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using APICarRegistration.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICarRegistration.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {}
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Model> Models { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Car> Cars { get; set; }
+    }
+}
