97c74df3159b251a52506ee44a0d9c011fbde237
Iago <iago0905@hotmail.com>
2023-07-12 21:54:27 -0300

    Implementação da solução, do projeto de teste e teste da classs BRANDS.

---
+++ APICarRegistration.Tests/BrandUnitTest.cs
@@ -0,0 +1,44 @@
+using APICarRegistration.Models;
+using APICarRegistration.Repositories.Interfaces;
+using FluentAssertions;
+
+namespace APICarRegistration.Tests
+{
+    public class BrandUnitTest
+    {
+        [Fact(DisplayName = "Create Brand With Valid State")]
+        public void CreateBrand_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Brand(1, "Kia");
+            action.Should()
+                .NotThrow<APICarRegistration.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create Brand With invalid State")]
+        public void CreateBrand_NegativeIdValue_ResultObjectInvalidState()
+        {
+            Action action = () => new Brand(-1, "Kia");
+            action.Should()
+                .Throw<APICarRegistration.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value");
+        }
+
+        [Fact(DisplayName = "Create Brand With Name too long")]
+        public void CreateBrand_LongNameValue_DomainExceptionLongName()
+        {
+            Action action = () => new Brand(1, "Kiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+            action.Should()
+                .Throw<APICarRegistration.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name, too long");
+        }
+
+        [Fact(DisplayName = "Create Brand With Invalid Name")]
+        public void CreateBrand_WithNullNameValue_DomainExceptionInvalidaName()
+        {
+            Action action = () => new Brand(1, "");
+            action.Should()
+                .Throw<APICarRegistration.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name.Name is required");
+        }
+    }
+}
---
+++ APICarRegistration/APICarRegistration/Models/Brand.cs
@@ -0,0 +1,49 @@
+﻿using APICarRegistration.Validation;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace APICarRegistration.Models
+{
+    [Table("Brands")]
+    public sealed class Brand
+    {
+        public Brand()
+        {
+            Models = new Collection<Model>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string? Name { get; set; }
+
+        [JsonIgnore]
+        public ICollection<Model>? Models{ get; set; }
+
+        public Brand(string? name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Brand(int id, string? name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name.Name is required");
+
+            DomainExceptionValidation.When(name.Length > 20, "Invalid name, too long");
+
+            Name = name;
+        }
+    }
+}
